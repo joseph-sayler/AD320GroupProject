aa1acc9ad1e69beadb6732ddfe8dca0e2ed262f0
Joseph Sayler <joseph.sayler@gmail.com>
2017-03-21 21:22:24 -0700

    fixed url for logoff
    had to add the following into a variable then double quote it with
    location: in front
    
    substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],"recipe/"))."recipe/index.php"

--- ajax_recipe/includes/login_registration/logoff.php
+++ ajax_recipe/includes/login_registration/logoff.php
@@ -1,14 +1,14 @@
 <?php
 
 $action = filter_input(INPUT_POST, "action");
-
+$location = substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],"recipe/"))."recipe/index.php";
 if ($action == 'logoff') {
     // clears cookies
     setcookie("loginForm1", '', time() - (300), "/");
     // unsets session
     session_unset();
     session_destroy();
-    header('location: '.substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],"/"))."index.php");
+    header("location: $location");
     exit();
 }
 
