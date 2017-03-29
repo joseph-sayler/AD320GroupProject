69fe7a0e193e68a8eaa4ac4f1b1032080c68e206
Joseph Sayler <joseph.sayler@gmail.com>
2017-03-28 21:03:42 -0700

    removed test files

--- test/backup/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require 'header.html';
-  include 'navbar.html';
-  include 'login.html';
-  include 'carousel.html';
-  include 'results.html';
-  include 'pager.html';
-  require 'footer.html';
-?>
